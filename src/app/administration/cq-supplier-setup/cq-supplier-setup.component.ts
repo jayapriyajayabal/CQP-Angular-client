7865bf3718257f1577764c4a4641e813fcd6e443
naveenannadurai <naveen.annadurai@lntinfotech.com>
2021-04-16 14:53:58 +0530

    Administration Page Updated

--- src/app/administration/cq-supplier-setup/cq-supplier-setup.component.ts
+++ src/app/administration/cq-supplier-setup/cq-supplier-setup.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CqSupplierSetupComponent implements OnInit {
 
+  public show:boolean = false;
+  public Search:any = 'Show';
   constructor() { }
 
   ngOnInit() {
+  }
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.Search = "Hide";
+    else
+      this.Search = "Show";
   }
 
 }
--- src/app/administration/user-role-mapping/user-role-mapping.component.ts
+++ src/app/administration/user-role-mapping/user-role-mapping.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-role-mapping.component.css']
 })
 export class UserRoleMappingComponent implements OnInit {
-
+  public show:boolean = false;
+  public Search:any = 'Show';
   constructor() { }
 
   ngOnInit() {
+  }
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.Search = "Hide";
+    else
+      this.Search = "Show";
   }
 
 }
