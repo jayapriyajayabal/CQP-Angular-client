cad52f135a4e1e45ccca7236578dbc1b39c8fea8
pryat <priyat0612@gmail.com>
2021-04-23 14:33:01 +0530

    added daily warranty page

--- src/app/warranty-claim/daily-warranty-claims/daily-warranty-claims.component.ts
+++ src/app/warranty-claim/daily-warranty-claims/daily-warranty-claims.component.ts
@@ -10,6 +10,35 @@ export class DailyWarrantyClaimsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  } 
+  items: any[] = [{id: 1, name: 'model1'}, {id: 2, name: 'model2'}, {id:3, name: 'model3'}];
+  selectedItems: any[] = [];
+  selectedToAdd: any[] = [];
+  selectedToRemove: any[] = [];
+
+  chosenItems(items){
+    this.selectedToAdd = items;
+  }
+
+  chosenItemsToRemove(items) {
+    this.selectedToRemove = items;
+  }
+
+  assigne() {
+    this.selectedItems = this.selectedItems.concat(this.selectedToAdd);
+    this.items = this.items.filter(item => {
+      return this.selectedItems.indexOf(item) < 0;
+    });
+
+    this.selectedToAdd = [];
+  }
+
+  unassigne() {
+    this.items = this.items.concat(this.selectedToRemove);
+    this.selectedItems = this.selectedItems.filter(item => {
+      return this.items.indexOf(item) < 0;
+    });
+    this.selectedToRemove = [];
   }
 
 }
